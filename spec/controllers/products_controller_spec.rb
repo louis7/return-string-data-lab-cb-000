5e8afbb5059d8676541e8be9b03dc7de90a6a819
louis baptiste <louisvis@ymail.com>
2020-05-28 20:40:44 +0000

    Automatically backed up by Learn

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
-  before do
-    Product.destroy_all
-    @product_attributes = {name: "Test", price: "3.99", inventory: 12, description: "This is a great thing"}
-  end
-
+  
   describe "POST create" do
 
     it 'creates a new product'
@@ -20,7 +16,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe "GET description" do
     it 'returns the description as a plain string' do
-      
+
     end
   end
 
